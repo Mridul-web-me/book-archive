9e595e66cbc07d1bf9db79dba929a5c6d491b9df
Mridul-web-me <md.mridu.23@gmail.com>
2021-09-02 10:18:29 +0600

    api updated

--- js/api.js
+++ js/api.js
@@ -1,6 +1,15 @@
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.visibility = displayStyle;
+}
+
 const searchBook = () => {
     const searchText = document.getElementById('search-field').value;
     loadBook(searchText);
+    document.getElementById('search-field').value = '';
+
+    toggleSpinner('initial');
+    document.getElementById('search-result').innerText = '';
+    document.getElementById('error-message').style.display = 'none';
 }
 
 const loadBook = searchText => {
@@ -13,18 +22,27 @@ const loadBook = searchText => {
 const displayLoad = books => {
     const container = document.getElementById('search-result');
     console.log(books);
-    books.forEach(book => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-            <img src="${book.cover_i}"
-            <div class="card-body">
-                <p class="card-text"> ${book.numFound}</p>
-                <h4 class="card-title">${book.title}</h4>
-                <h5 class="card-title">${book.author_name}</h5>
-                <p class="card-text"> ${book.first_publish_year}</p>
-        </div>
-        `;
-        container.appendChild(div);
-    });
+    container.textContent = '';
+    document.getElementById('search-number').innerText = `${books.docs}`;
+
+    if(books.length === 0){
+        document.getElementById('error-message').style.display = 'block'
+    }
+    else{
+        books.forEach(book => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div class="card-body">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="...">
+                   
+                    <h4 class="card-title">${book.title}</h4>
+                    <h5 class="card-title">${book.author_name}</h5>
+                    <p class="card-text"> ${book.first_publish_year}</p>
+            </div>
+            `;
+            container.appendChild(div);
+        });
+    }
+    toggleSpinner('hidden');
 }
